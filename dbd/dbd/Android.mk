acba0e15df7bb3fbcefea8aaa586dddc21107fa1
duongbaoduy <baoduy.duong0206@gmail.com>
2014-04-11 10:00:54 +0700

    Signed-off-by: duongbaoduy <baoduy.duong0206@gmail.com>

--- dbd/dbd/Android.mk
+++ dbd/dbd/Android.mk
@@ -3,20 +3,20 @@
 LOCAL_PATH := $(call my-dir)
 #
 # this is here to use the pre-built kernel
-#ifeq ($(TARGET_PREBUILT_KERNEL),)
-#	TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-#endif
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+endif
 #
-#file := $(INSTALLED_KERNEL_TARGET)
-#ALL_PREBUILT += $(file)
-#$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-#	$(transform-prebuilt-to-target)
-#
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
+
 # no boot loader, so we don't need any of that stuff..  
 #
-#LOCAL_PATH := vendor/dbd/dbd
+LOCAL_PATH := vendor/dbd/dbd
 #
-#include $(CLEAR_VARS)
+include $(CLEAR_VARS)
 #
 # include more board specific stuff here? Such as Audio parameters.      
 #
--- dbd/products/emu.mk
+++ dbd/products/emu.mk
@@ -1,8 +1,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/board/generic/device.mk)
+
+#include $(SRC_TARGET_DIR)/product/emulator.mk
 
 #
 # Overrides
 PRODUCT_NAME := emu
 PRODUCT_DEVICE := dbd
-PRODUCT_BRAND := dbd Android
+PRODUCT_BRAND := Android-dbd
 PRODUCT_MODEL := AOSP on ARM Emulator
+
+LOCAL_KERNEL := prebuilts/qemu-kernel/arm/kernel-qemu-armv7
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
